e87608136572f9f9e1be128d86811b01f03bfe90
Viacheslaw Saveliew <version-1@yandex.ru>
2022-12-05 21:16:08 +0300

    add vite

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
+import './style.css'
+
 console.log('500')
 
 // resize listener
 window.onresize = resize;
 resize(); // init
 
-function resize () {
-    let fieldHeight = window.innerHeight
-    let fieldWidth = window.innerWidth
-    console.log('height:', fieldHeight, 'width:', fieldWidth)
+function resize() {
+  let fieldHeight = window.innerHeight
+  let fieldWidth = window.innerWidth
+  console.log('height:', fieldHeight, 'width:', fieldWidth)
 }
 
 // Canvas init
@@ -15,7 +17,7 @@ const canvas = document.getElementById('playground');
 const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = "rgb(200,0,0)";
-ctx.fillRect (10, 10, 55, 50);
+ctx.fillRect(10, 10, 55, 50);
 
 ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.fillRect (30, 30, 55, 50);
+ctx.fillRect(30, 30, 55, 50);
